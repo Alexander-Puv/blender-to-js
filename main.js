2f02ae11221c5954e793d901f167fe1f2ec94c2b
Alexander-Puv <sansano4sana@gmail.com>
2023-01-04 21:34:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import './style.css'
+
+function init() {
+  // Set up the scene, camera, and renderer
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0x000000);
+  document.body.appendChild(renderer.domElement);
+  
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+  
+  camera.position.z = 200;
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.campingFactor = 0.25;
+  controls.enableZoom = true;
+
+  var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+  keyLight.position.set(-100, 0, 100);
+
+  var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+  fillLight.position.set(100, 0, 100);
+
+  var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+  backLight.position.set(100, 0, -100).normalize();
+
+  scene.add(keyLight);
+  scene.add(fillLight);
+  scene.add(backLight);
+
+  const objLoader = new OBJLoader()
+  objLoader.setPath('./assets/')
+  objLoader.load('xyz.obj', function(object) {
+    object.position.y -= 60;
+    scene.add(object)
+  })
+  // Load the OBJ file and add it to the scene
+/*   const objLoader = new OBJLoader();
+  let obj
+  objLoader.load(
+    './assets/xyz.obj',
+    function (object) {
+      // change sizes
+      object.scale.set(2, 2, 2);
+      obj = object
+      scene.add(object);
+    }
+  ); */
+  
+  function animate() {
+    requestAnimationFrame(animate);
+    
+    controls.update()
+
+    renderer.render(scene, camera);
+  }
+  animate();
+}
+
+window.onload = init;
